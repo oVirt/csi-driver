9aac2d8c583fb4d85c103d15ab48c9b2d8185ab2
Roy Golan <rgolan@redhat.com>
2020-01-07 18:41:20 +0200

    Helper to extract disk attachments by VM id
    Signed-off-by: Roy Golan <rgolan@redhat.com>

---
+++ pkg/service/helper.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"fmt"
+
+	ovirtsdk "github.com/ovirt/go-ovirt"
+)
+
+func diskAttachmentByVmAndDisk(connection *ovirtsdk.Connection, vmId string, diskId string) (*ovirtsdk.DiskAttachment, error) {
+	vmService := connection.SystemService().VmsService().VmService(vmId)
+	attachments , err := vmService.DiskAttachmentsService().List().Send()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, attachment := range attachments.MustAttachments().Slice() {
+		if diskId == attachment.MustDisk().MustId() {
+			return attachment, nil
+		}
+	}
+	return nil, fmt.Errorf("failed to find attachment by disk %s for VM %s", diskId, vmId)
+}
